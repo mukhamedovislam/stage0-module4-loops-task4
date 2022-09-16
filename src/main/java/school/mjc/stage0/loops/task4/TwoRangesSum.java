06bde6a7044076c08ea3b76e2eb72b4c187a57e5
mukhamedovislam <bozuy24@gmail.com>
2022-09-16 07:47:55 +0400

    Fixed Loop Keywords

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+	                    int skippedSum = 0, countedSum = 0;
+            if(numberToSkip > lastInRow){
+                    System.out.println("number to skip is bugger than the last");
+            } else if(lastInRow < 0){
+                    System.out.println("last number in row is negative");
+            } else if(numberToSkip == lastInRow){
+              for(int i = 1; i <= lastInRow; i++){
+                skippedSum += i;
+              }
+              System.out.println("skipped sum is " + skippedSum + "\ncounted sum is " + countedSum);
+            } else{
+                for(int i = 1; i <= lastInRow; i++){
+                        if(i % numberToSkip == 0){
+                                skippedSum += i;
+                                continue;
+                        }
+                        countedSum += i;
+                }
+                System.out.println("skipped sum is " + skippedSum + "\ncounted sum is " + countedSum);
+            }
     }
 }
